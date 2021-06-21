aa4366446deb784eb22f2ec4e519d64740a1b6f4
TRBhavaniShankar <tr.bhavanishankar93@gmail.com>
2021-06-21 00:44:38 -0230

    fetch json

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 const express = require('express')
 const app = express()
 const port = 4433
+const fetch = require('node-fetch');
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/getPlayersList', (req, res) => {
+    // var playersListJson = 
+    let url = "https://playline-dev-test.s3-us-west-2.amazonaws.com/playline-test.json";
+    let settings = { method: "Get" };
+
+    
+
+    fetch(url, settings)
+      .then(res => res.json())
+      .then((json) => {
+          res.send(json)
+      });
+    
+    
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
